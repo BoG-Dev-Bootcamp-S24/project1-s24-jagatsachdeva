fdde6c104cc01138f2564a0757e644418e8c5cd4
jsachdeva7 <jsachdeva7@gatech.edu>
2024-02-29 18:38:24 -0500

    Built filtering and rendering functionality for the train data.

--- marta-interface/src/pages/TrainList.js
+++ marta-interface/src/pages/TrainList.js
@@ -13,7 +13,7 @@ export const TrainList = () => {
 
     // fetch for stations
     useEffect(() => {
-        fetch(`https://midsem-bootcamp-api.onrender.com/stations/${currentColor.toUpperCase()}`)
+        fetch(`https://midsem-bootcamp-api.onrender.com/stations/${currentColor}`)
             .then(res => res.json())
             .then((stations) => {
                 setStations(stations);
@@ -22,7 +22,7 @@ export const TrainList = () => {
 
     // fetch for data
     useEffect(() => {
-        fetch(`https://midsem-bootcamp-api.onrender.com/arrivals/${currentColor.toUpperCase()}`)
+        fetch(`https://midsem-bootcamp-api.onrender.com/arrivals/${currentColor}`)
             .then(res => res.json())
             .then((data) => {
                 setData(data);
@@ -31,29 +31,12 @@ export const TrainList = () => {
     }, [currentColor]);
 
     useEffect(() => {
-        console.log(data);
-    },[data])
-
-    useEffect(() => {
         if (currentColor === "red" || currentColor === "gold") {
             setBoundButtons(["Northbound", "Southbound"]);
         } else if (currentColor === "green" || currentColor === "blue") {
             setBoundButtons(["Eastbound", "Westbound"]);
         }
     }, [currentColor]);
-
-    useEffect(() => {
-        console.log("Directions: " + boundButtons);
-    }, [boundButtons]);
-
-    useEffect(() => {
-        console.log(currentFilter);
-    }, [currentFilter]);
-
-    useEffect(() => {
-        console.log("Current Station: " + currentStationFilter);
-    }, [currentStationFilter]);
-
 
     const renderStations = () => {
         if (!stations) {
@@ -79,38 +62,136 @@ export const TrainList = () => {
         return (buttonName);
     }
 
-    // const renderTrains = () => {
-    //     // filters: starting station, direction
-    //     let directionFilter = false;
-    //     let delayFilter = false;
+    const renderTrains = () => {
+        // filters: starting station, direction
+        let directionFilter = false;
+        let delayFilter = false;
+        let updatingFilteredData = [];
+    
+        if (currentFilter === "Arriving" || currentFilter === "Scheduled") {
+            delayFilter = true;
+        } else if (currentFilter === "Northbound" || currentFilter === "Southbound" || currentFilter === "Eastbound" || currentFilter === "Westbound") {
+            directionFilter = true;
+        }
+    
+        let filteredData = data.filter(currentTrain => {
+            let returnDirectionValue;
+            let returnStationValue;
+            let nonDuplicate = true;
+            if (delayFilter) {
+                if (currentFilter === "Arriving") {
+                    returnDirectionValue = currentTrain.DELAY === "T0S";
+                } else if (currentFilter === "Scheduled") {
+                    returnDirectionValue = currentTrain.DELAY !== "T0S";
+                }
+            } else if (directionFilter) {
+                if (currentColor === "gold" || currentColor === "red") {
+                    if (currentFilter === "Northbound") {
+                        returnDirectionValue = currentTrain.DIRECTION === "N";
+                    } else if (currentFilter === "Southbound") {
+                        returnDirectionValue = currentTrain.DIRECTION === "S";
+                    }
+                } else if (currentColor === "blue" || currentColor === "green") {
+                    if (currentFilter === "Eastbound") {
+                        returnDirectionValue = currentTrain.DIRECTION === "E";
+                    } else if (currentFilter === "Westbound") {
+                        returnDirectionValue = currentTrain.DIRECTION === "W";
+                    }
+                }
+            }
 
-    //     if (currentFilter === "Arriving" || currentFilter === "Scheduled") {
-    //         delayFilter = true;
-    //     } else if (currentFilter === "Northbound" || currentFilter === "Southbound" || currentFilter === "Eastbound" || currentFilter === "Westbound") {
-    //         directionFilter = true;
-    //     } 
+            updatingFilteredData.forEach(currentFilteredTrain => {
+                let comparingWaitingTime;
+                if (currentTrain.WAITING_TIME === "Arriving") {
+                    if (currentFilteredTrain.WAITING_TIME === "Arriving") {
+                        comparingWaitingTime = true;
+                    }
+                } else if (Math.abs(parseInt(currentFilteredTrain.WAITING_TIME.split()[0]) - parseInt(currentTrain.WAITING_TIME.split()[0])) <= 5) {
+                    comparingWaitingTime = true;
+                }
+                if (comparingWaitingTime && currentFilteredTrain.STATION === currentTrain.STATION) {
+                    nonDuplicate = false;
+                }
+            })
+            
+            if (currentStationFilter !== "All Stations") {
+                if (currentStationFilter === "Lakewood/Ft. McPherson") {
+                    returnStationValue = currentTrain.STATION === "LAKEWOOD STATION";
+                } else if (currentStationFilter === "Hamilton E. Holmes") {
+                    returnStationValue = currentTrain.STATION === "HE HOLMES STATION";
+                } else {
+                    returnStationValue = currentTrain.STATION === (currentStationFilter.toUpperCase() + " STATION");
+                }
+            } else {
+                returnStationValue = true;
+            }
+            // console.log("returnDirectionValue:");
+            // console.log(returnDirectionValue);
+            // // console.log("returnDelayValue");
+            // // console.log(returnDelayValue);
+            // console.log("returnStationValue:");
+            // console.log(returnStationValue);
+            if (returnDirectionValue && returnStationValue) {
+                updatingFilteredData.push(currentTrain);
+            }
+            return returnDirectionValue && returnStationValue && nonDuplicate;
+        });
 
-    //     filteredData = data.filter(currentTrain => {
-    //         let returnDirectionValue;
-    //         let returnStationValue;
-    //         if (delayFilter) {
-    //             currentFilter === "Arriving" ? returnDirectionValue = currentTrain.DELAY === "TOS" : returnDirectionValue = currentTrain.DELAY !== "TOS";
-    //         } else if (directionFilter) {
-    //             if (currentColor === "gold" || currentColor === "red") {
-    //                 currentFilter === "Northbound" ? returnDirectionValue = currentTrain.DIRECTION === "N" : returnDirectionValue = currentTrain.DIRECTION !== "S";
-    //             } else {
-    //                 currentFilter === "Eastbound" ? returnDirectionValue = currentTrain.DIRECTION === "E" : returnDirectionValue = currentTrain.DIRECTION !== "W";
-    //             }
-    //         }
-    //         currentStationFilter !== "All Stations" ? returnStationValue = currentTrain.STATION === currentStationFilter.toUpperCase : returnStationValue = true;
-    //         return returnDirectionValue && returnStationValue;
-    //     })
+        if (filteredData.length === 0) {
+            return (<div id="loading"><p>There are no trains to display.</p></div>)
+        }
 
-    //     return (
-
-    //     )
-        
-    // }
+        console.log("filteredData:")
+        console.log(filteredData);
+    
+        return (
+            filteredData.map((currentTrain, index) => (
+                <div key={index} className="train">
+                    <div className="logo"></div>
+                    <div className="infoBox">
+                        <div className="destination">
+                            <p><span className="destinationText">{currentTrain.STATION}</span> <span>&#8594;</span> <span className="destinationText">{currentTrain.DESTINATION}</span></p>
+                        </div>
+                        <div className="details">
+                        <button className="tag lineColorTag" style={{
+                            backgroundColor: (() => {
+                                if (currentColor === "gold") {
+                                return "rgb(255, 215, 0)";
+                                } else if (currentColor === "red") {
+                                return "rgb(228, 78, 78)";
+                                } else if (currentColor === "blue") {
+                                return "rgb(81, 81, 249)";
+                                } else if (currentColor === "green") {
+                                return "rgb(54, 151, 54)";
+                                }
+                            })()
+                            }}>
+                            {currentColor.toUpperCase()}
+                        </button>
+                            <button className="tag delayedTag" style={{
+                            backgroundColor: (() => {
+                                if (currentTrain.DELAY === "T0S") {
+                                    return "rgb(149, 227, 149)";
+                                } else {
+                                    return "rgb(244, 141, 129)";
+                                } 
+                            })()
+                            }}>
+                                {(() => {
+                                    if (currentTrain.DELAY === "T0S") {
+                                        return "ON TIME";
+                                    } else {
+                                    return "DELAYED";
+                                    }
+                                })()}
+                            </button>
+                            <button className="tag timeTag">{(currentTrain.WAITING_TIME).toUpperCase()}</button>
+                        </div>
+                    </div>
+                </div>
+            ))
+        );
+    }
 
     return (
         <div  id="TrainList">
@@ -130,71 +211,7 @@ export const TrainList = () => {
                     </div>
                     <div class="divider"></div>
                     {!isPending && <div id="dataBox">
-                        <div className="train">
-                            <div className="logo"></div>
-                            <div className="infoBox">
-                                <div className="destination">
-                                    <p><span className="destinationText">Brookhaven Station</span> <span>&#8594;</span> <span className="destinationText">Doraville</span></p>
-                                </div>
-                                <div className="details">
-                                    <button className="tag lineColorTag">Gold</button>
-                                    <button className="tag delayedTag">Delayed</button>
-                                    <button className="tag timeTag">20 min</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="train">
-                            <div className="logo"></div>
-                            <div className="infoBox">
-                                <div className="destination">
-                                    <p><span className="destinationText">Brookhaven Station</span> <span>&#8594;</span> <span className="destinationText">Doraville</span></p>
-                                </div>
-                                <div className="details">
-                                    <button className="tag lineColorTag">Gold</button>
-                                    <button className="tag delayedTag">Delayed</button>
-                                    <button className="tag timeTag">20 min</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="train">
-                            <div className="logo"></div>
-                            <div className="infoBox">
-                                <div className="destination">
-                                    <p><span className="destinationText">Brookhaven Station</span> <span>&#8594;</span> <span className="destinationText">Doraville</span></p>
-                                </div>
-                                <div className="details">
-                                    <button className="tag lineColorTag">Gold</button>
-                                    <button className="tag delayedTag">Delayed</button>
-                                    <button className="tag timeTag">20 min</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="train">
-                            <div className="logo"></div>
-                            <div className="infoBox">
-                                <div className="destination">
-                                    <p><span className="destinationText">Brookhaven Station</span> <span>&#8594;</span> <span className="destinationText">Doraville</span></p>
-                                </div>
-                                <div className="details">
-                                    <button className="tag lineColorTag">Gold</button>
-                                    <button className="tag delayedTag">Delayed</button>
-                                    <button className="tag timeTag">20 min</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="train">
-                            <div className="logo"></div>
-                            <div className="infoBox">
-                                <div className="destination">
-                                    <p><span className="destinationText">Brookhaven Station</span> <span className="arrow">&#8594;</span> <span className="destinationText">Doraville</span></p>
-                                </div>
-                                <div className="details">
-                                    <button className="tag lineColorTag">Gold</button>
-                                    <button className="tag delayedTag">Delayed</button>
-                                    <button className="tag timeTag">20 min</button>
-                                </div>
-                            </div>
-                        </div>
+                        {renderTrains()}
                     </div>}
                     {isPending && <div id="loading"><p>Loading trains...</p></div>}
                 </div>
